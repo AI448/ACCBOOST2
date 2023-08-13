2fdbe1334d0b390e4ee206b7f853a0605a697435
imai <imai448@gmail.com>
2023-08-13 12:53:15 +0900

    テスト結果をリポジトリに追加

--- tests/utility/iterable/test_chain.cpp
+++ tests/utility/iterable/test_chain.cpp
@@ -12,6 +12,7 @@ int main()
   {
     auto a = range(3);
     auto b = range(5);
+    static_assert(std::input_iterator<decltype(chain(a, b).begin())>);
     TEST_UTILS::dump(chain(a, b));
   }
 
--- tests/utility/iterable/test_zip.cpp
+++ tests/utility/iterable/test_zip.cpp
@@ -7,6 +7,7 @@ int main()
 {
   std::vector<int> a = {1, 2, 3};
   std::vector<double> b = {0.1, 0.2, 0.3};
+  static_assert(std::ranges::range<decltype(ACCBOOST2::zip(a, b))>);
   TEST_UTILS::dump(ACCBOOST2::zip(a, b));
 
   return 0;
